d5b8b0027ea3e2660c1b5065e91ac519c023e72c
Tade <tadew@live.com>
2024-12-30 09:01:13 +1000

    Removed Zoxide

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -208,17 +208,3 @@ function Get-Theme {
     }
 }
 Get-Theme
-
-## Final Line to set prompt
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
-} else {
-    Write-Host "zoxide command not found. Attempting to install via winget..."
-    try {
-        winget install -e --id ajeetdsouza.zoxide
-        Write-Host "zoxide installed successfully. Initializing..."
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
-    } catch {
-        Write-Error "Failed to install zoxide. Error: $_"
-    }
-}
--- setup.ps1
+++ setup.ps1
@@ -116,11 +116,3 @@ try {
 catch {
     Write-Error "Failed to install Terminal Icons module. Error: $_"
 }
-# zoxide Install
-try {
-    winget install -e --id ajeetdsouza.zoxide
-    Write-Host "zoxide installed successfully."
-}
-catch {
-    Write-Error "Failed to install zoxide. Error: $_"
-}
